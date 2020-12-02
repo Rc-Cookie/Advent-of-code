486ada34f885a47bdb8c36c60f3758f9126813f8
RcCookie0 <70144256+RcCookie0@users.noreply.github.com>
2020-12-02 16:46:36 +0100

    Added date getter

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.Calendar;
+
 public class Main {
     public static void main(String[] args) {
+        String day = "day" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        try{
+            Main.class.getDeclaredMethod(day, String[].class);
+        } catch (NoSuchMethodException e) { }
+    }
+
+    public void day1(String[] args) {
         
     }
 }
