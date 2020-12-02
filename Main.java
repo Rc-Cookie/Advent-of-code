9f44347e93a2ed9e39edbcdb563427cbc8f88e27
RcCookie0 <70144256+RcCookie0@users.noreply.github.com>
2020-12-02 19:19:43 +0100

    Fixed method call

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
-import java.util.Calendar;
-
 public class Main {
     public static void main(String[] args) {
-        String day = "day" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-        try{
-            Main.class.getDeclaredMethod(day, String[].class);
-        } catch (NoSuchMethodException e) { }
+        day1(args);
     }
 
-    public void day1(String[] args) {
+
+    
+    public static void day1(String[] args) {
         
     }
 }
