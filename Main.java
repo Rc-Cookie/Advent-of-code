684136a110db6172d28e580b9b1e8b49a43aa697
RcCookie0 <70144256+RcCookie0@users.noreply.github.com>
2020-12-02 20:07:10 +0100

    Added code for day 1 and 2

--- Main.java
+++ Main.java
@@ -1,11 +1,103 @@
+import java.io.File;
+import java.util.Scanner;
+
+import rccookie.util.Console;
+
 public class Main {
     public static void main(String[] args) {
-        day1(args);
+        try {
+            day2_2(args);
+        } catch (Exception e) { e.printStackTrace(); }
     }
 
 
-    
-    public static void day1(String[] args) {
+
+
+
+
+
+
+    public static void day2_1(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("data/day2.passwords"));
         
+        int valid = 0;
+
+        while(sc.hasNextLine()) {
+            StringBuilder line = new StringBuilder(sc.nextLine());
+            
+            int min = Integer.parseInt(line.substring(0, line.indexOf("-")));
+            int max = Integer.parseInt(line.substring(line.indexOf("-") + 1, line.indexOf(" ")));
+            line.delete(0, line.indexOf(" ") + 1);
+
+            char c = line.charAt(0);
+            line.delete(0, line.indexOf(" ") + 1);
+            
+            int count = 0;
+            for(int i=0; i<line.length(); i++) {
+                if(line.charAt(i) == c) count++;
+            }
+
+            if(count >= min && count <= max) valid++;
+        }
+
+        Console.log("Valid: " + valid);
+    }
+
+    public static void day2_2(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("data/day2.passwords"));
+        
+        int valid = 0;
+
+        while(sc.hasNextLine()) {
+            StringBuilder line = new StringBuilder(sc.nextLine());
+            
+            int a = Integer.parseInt(line.substring(0, line.indexOf("-")));
+            int b = Integer.parseInt(line.substring(line.indexOf("-") + 1, line.indexOf(" ")));
+            line.delete(0, line.indexOf(" ") + 1);
+
+            char c = line.charAt(0);
+            line.delete(0, line.indexOf(" ")); // No +1 means first character is automatically '1'
+            
+            if(line.length() <= a) continue;
+            if(line.charAt(a) == c) {
+                if(line.length() <= b || line.charAt(b) != c) valid++;
+            }
+            else if(line.length() > b && line.charAt(b) == c) valid++;
+        }
+
+        Console.log("Valid: " + valid);
+    }
+
+
+
+
+
+
+    public static void day1_1(String[] args) {
+        int[] numbers = new int[args.length];
+        for(int i=0; i<numbers.length; i++) numbers[i] = Integer.parseInt(args[i]);
+
+        for(int i=0; i<numbers.length-1; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                if(numbers[i] + numbers[j] == 2020) {
+                    Console.log(numbers[i], numbers[j], numbers[i] * numbers[j]);
+                }
+            }
+        }
+    }
+
+    public static void day1_2(String[] args) {
+        int[] numbers = new int[args.length];
+        for(int i=0; i<numbers.length; i++) numbers[i] = Integer.parseInt(args[i]);
+
+        for(int i=0; i<numbers.length-2; i++) {
+            for(int j=i+1; j<numbers.length-1; j++) {
+                for(int k=j+1; k<numbers.length; k++) {
+                    if(numbers[i] + numbers[j] + numbers[k] == 2020) {
+                        Console.log(numbers[i], numbers[j], numbers[k], numbers[i] * numbers[j] * numbers[k]);
+                    }
+                }
+            }
+        }
     }
 }
