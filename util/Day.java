867b9d5d1a136388a565ccc175e96fa72213f54e
RcCookie <70144256+RcCookie0@users.noreply.github.com>
2020-12-03 16:53:55 +0100

    Added getInput

--- util/Day.java
+++ util/Day.java
@@ -1,10 +1,24 @@
 package util;
 
+import static java.nio.StandardCharsets.US_ASCII;
+import static java.util.Files.readString;
+
 public abstract class Day {
+
     public abstract void run1(String[] args) throws Exception;
+
     public void run2(String[] args) throws Exception {
         throw new NotImplementedException();
     };
+
+    protected String getInput() {
+        try {
+            int day = Integer.parseInt(getClass().getSimpleName().subString(3));
+            return readString("data/day" + day + ".input", US_ASCII);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public class NotImplementedException extends Exception {
         private static final long serialVersionUID = -4881948333783821683L;
