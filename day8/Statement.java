a6289020ef8eb1d57bf39797e585f2badbf6ca35
RcCookie0 <70144256+RcCookie0@users.noreply.github.com>
2020-12-08 15:07:07 +0100

    Added day 8 part 1

---
+++ day8/Statement.java
@@ -0,0 +1,31 @@
+package day8;
+
+public class Statement {
+
+    public static int ACCUMULATION = 0;
+    
+    private int callCount = 0;
+
+    private final int accumulation;
+    private final int nextIndex;
+
+    public Statement(String function, int argument, int index) {
+        accumulation = "acc".equals(function) ? argument : 0;
+        nextIndex = index + ((accumulation == 0 && "jmp".equals(function)) ? argument : 1);
+    }
+
+    public int getCallCount() {
+        return callCount;
+    }
+
+    public Statement process(Statement[] statements) {
+        callCount++;
+        ACCUMULATION += accumulation;
+        return statements[nextIndex/* % statements.length*/];
+    }
+
+    @Override
+    public String toString() {
+        return "acc " + accumulation + ", jump to " + nextIndex;
+    }
+}
